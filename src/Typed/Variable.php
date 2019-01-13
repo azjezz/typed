faedc77848e8a7b4c341ecff13df3bd270d631c2
azjezz <29315886+azjezz@users.noreply.github.com>
2019-01-13 03:04:51 +0100

    Update Variable.php

--- src/Typed/Variable.php
+++ src/Typed/Variable.php
@@ -25,85 +25,85 @@ final class Variable
     Repository::add($this);
   }
   
-  public static function string(string $value = ''): string
+  public static function &string(string $value = ''): string
   {
      $var = new self();
      $var->string = $value;
-     return &$var->string;
+     return $var->string;
   }
   
-  public static function float(float $value = 0.0): float
+  public static function &float(float $value = 0.0): float
   {
     $var = new self();
     $var->float = $value;
-    return &$var->float;
+    return $var->float;
   }
   
-  public static function int(int $value = 0): int
+  public static function &int(int $value = 0): int
   {
     $var = new self();
     $var->int = $value;
-    return &$var->int;
+    return $var->int;
   }
   
-  public static function array(array $value = []): array
+  public static function &array(array $value = []): array
   {
     $var = new self();
     $var->array = $value;
-    return &$var->array;
+    return $var->array;
   }
   
-  public static function bool(bool $value = false): bool
+  public static function &bool(bool $value = false): bool
   {
     $var = new self();
     $var->bool = $value;
-    return &$var->bool;
+    return $var->bool;
   }
   
-  public static function iterable(iterable $value = []): iterable
+  public static function &iterable(iterable $value = []): iterable
   {
     $var = new self();
     $var->iterable = $value;
-    return &$var->iterable;
+    return $var->iterable;
   }
   
-  public static function stdClass(?stdClass $value = null): stdClass
+  public static function &stdClass(?stdClass $value = null): stdClass
   {
     $value ??= new stdClass();
     $var = new self();
     $var->stdClass = $value;
-    return &$var->stdClass;
+    return $var->stdClass;
   }
   
-  public static function object(?object $value = null): object
+  public static function &object(?object $value = null): object
   {
     $value ??= new stdClass();
     $var = new self();
     $var->object = $value;
-    return &$var->object;
+    return $var->object;
   }
   
-  public static function callable(?callable $value = null): callable
+  public static function &callable(?callable $value = null): callable
   {
     $value ??= [static::class, 'callable'];
     $var = new self();
     $var->callable = $value;
-    return &$var->callable;
+    return $var->callable;
   }
   
-  public static function countable(?Countable $value = null): Countable
+  public static function &countable(?Countable $value = null): Countable
   {
     $value ??= new ArrayObject();
     $var = new self();
     $var->countable = $value;
-    return &$var->countable;
+    return $var->countable;
   }
   
-  public static function traversable(?Traversable $value = null): Traversable
+  public static function &traversable(?Traversable $value = null): Traversable
   {
     $value ??= new ArrayObject();
     $var = new self();
     $var->traversable = $value;
-    return &$var->traversable;
+    return $var->traversable;
   }
 }
