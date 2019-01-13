406edc77d5de189468d7a000468b72f065b31862
azjezz <29315886+azjezz@users.noreply.github.com>
2019-01-13 02:16:42 +0100

    Create Repository.php

---
+++ src/Typed/Repository.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Typed;
+
+class Repository 
+{
+    private static array $variables = [];
+    private function __construct() {}
+    public static function add(Variable $var): void 
+    {
+        static::$variables[] = $var;
+    }
+}
